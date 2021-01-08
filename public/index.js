afd7b26da898d7e2e77e557b8914b41efe9f590d
simarbagga401 <simarbagga401@gmail.com>
2021-01-08 17:51:32 +0530

    :tada: Room Joining Feature Added

--- public/index.js
+++ public/index.js
@@ -5,9 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
+const uuid_1 = require("uuid");
 const app = express_1.default();
 app.use(cors_1.default());
-app.get('/', (_, res) => {
+app.get("/", (_, res) => {
     res.send("Hello There!");
 });
 const server = app.listen(8000, () => console.log(" App Running on PORT 8000"));
@@ -15,11 +16,53 @@ const io = require("socket.io")(server, {
     cors: {
         origin: "http://localhost:5500",
         methods: ["GET", "POST"],
-    }
+    },
 });
-io.sockets.on('connection', (socket) => {
-    console.log('New Connection', socket.id);
-    socket.on('snake_position', ([X, Y]) => {
-        console.log(`x : ${X} , y : ${Y}`);
+let rooms = [];
+io.sockets.on("connection", (connection) => {
+    console.log("New Connection :- ", connection.id);
+    const room_id = uuid_1.v4();
+    rooms.push({
+        room_id,
+        clients: [connection.id],
+    });
+    let payload = {
+        client_id: connection.id,
+        room_id,
+    };
+    connection.emit("game_init", payload);
+    if (payload)
+        console.log(payload);
+    connection.on("enter_game_req", ({ client_id, room_id }) => {
+        let payload;
+        let room = rooms.find((r) => r.room_id === room_id);
+        let room_creator = room === null || room === void 0 ? void 0 : room.clients[0];
+        if (!room)
+            payload = { client: "Requestor", err_msg: "Room Not Found" };
+        else if (room.clients.length > 2)
+            payload = { client: "Requestor", err_msg: "Room Limit Reached" };
+        else {
+            room.clients.push(client_id);
+            payload = Object.assign({ client: "Requestor" }, room);
+        }
+        connection.emit("enter_game_res", payload);
+        connection.to(room_creator).emit("enter_game_res", Object.assign({ client: "Creator" }, room));
+    });
+    // connection.on("");
+    // connection.on("snake_pos_client", ({x,y}:{x:Number,y:Number}) => {
+    //     let clients_in_game = games.forEach(c => )
+    //     connection.broadcast.emit("snake_pos_server",{x,y})
+    // })
+    connection.on("outgoing_chat", ({ client_id, room_id, chat, }) => {
+        let room = rooms.filter((r) => r.room_id === room_id);
+        let is_client_in_room = room.clients.includes(client_id);
+        let payload = {
+            client_id,
+            chat,
+        };
+        room.clients.forEach((c) => {
+            if (c != room.clients[0])
+                connection.to(c).emit("incomming_chat", payload);
+        });
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,91 @@
-import express from 'express';
-import cors from 'cors';
-import socket from 'socket.io';
+import express from "express";
+import cors from "cors";
+import { v4 as uuid } from "uuid";
 const app = express();
 app.use(cors());
-app.get('/', (_,res) => {
-    res.send("Hello There!")
+app.get("/", (_, res) => {
+  res.send("Hello There!");
 });
-
 const server = app.listen(8000, () => console.log(" App Running on PORT 8000"));
 const io = require("socket.io")(server, {
-    cors: {
-        origin: "http://localhost:5500",
-        methods: ["GET", "POST"],
-  }});
-io.sockets.on('connection', (socket:any) => {
-    console.log('New Connection', socket.id);
-    socket.on('snake_position',([X,Y]:[X:number,Y:number]) => {
-        console.log(`x : ${X} , y : ${Y}`);
-    });
+  cors: {
+    origin: "http://localhost:5500",
+    methods: ["GET", "POST"],
+  },
+});
+
+interface Room {
+  room_id: string;
+  clients: Array<string>;
+}
+
+let rooms: Room[] = [];
+io.sockets.on("connection", (connection: any) => {
+  console.log("New Connection :- ", connection.id);
+  const room_id: string = uuid();
+
+  rooms.push({
+    room_id,
+    clients: [connection.id],
+  });
+
+  let payload = {
+    client_id: connection.id,
+    room_id,
+  };
+  connection.emit("game_init", payload);
+  if (payload) console.log(payload);
+  connection.on(
+    "enter_game_req",
+    ({ client_id, room_id }: { client_id: string; room_id: string }) => {
+      let payload;
+      let room = rooms.find((r) => r.room_id === room_id);
+      let room_creator = room?.clients[0];
+      if (!room) payload = { client: "Requestor", err_msg: "Room Not Found" };
+      else if (room.clients.length > 2)
+        payload = { client: "Requestor", err_msg: "Room Limit Reached" };
+      else {
+        room.clients.push(client_id);
+        payload = {
+          client: "Requestor",
+          ...room,
+        };
+      }
+
+      connection.emit("enter_game_res", payload);
+      connection.to(room_creator).emit("enter_game_res", {
+        client: "Creator",
+        ...room,
+      });
+    }
+  );
+  // connection.on("");
+  // connection.on("snake_pos_client", ({x,y}:{x:Number,y:Number}) => {
+  //     let clients_in_game = games.forEach(c => )
+  //     connection.broadcast.emit("snake_pos_server",{x,y})
+  // })
+
+  connection.on(
+    "outgoing_chat",
+    ({
+      client_id,
+      room_id,
+      chat,
+    }: {
+      client_id: string;
+      room_id: string;
+      chat: string;
+    }) => {
+      let room = rooms.filter((r) => r.room_id === room_id);
+      let is_client_in_room = room.clients.includes(client_id);
+      let payload = {
+        client_id,
+        chat,
+      };
+      room.clients.forEach((c) => {
+        if (c != room.clients[0])
+          connection.to(c).emit("incomming_chat", payload);
+      });
+    }
+  );
 });
